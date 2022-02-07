780f74ab8769e13d2b1596f1b9267f7d8d0dc3ba
ChengxuLee <1695172472@qq.com>
2022-02-07 13:09:17 +0800

    leetcode

---
+++ leetcode/完整/leetcode 1. 两数之和.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+using namespace std;
+/*给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+示例 1：
+
+输入：nums = [1,1,2]
+输出：2, nums = [1,2]
+解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+*/
+#include <vector>
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+          int first=1;
+          int second=1;
+          if(nums.empty()) return 0;
+            while(second<nums.size()){
+                if(nums[second] != nums[second-1]){
+                    nums[first++]=nums[second]; 
+                }
+                second++;
+            }
+          return first;
+    }
+};
+int main() {
+
+
+	system("pause");
+	return 0;
+}
---
+++ leetcode/完整/leetcode 26. 删除有序数组中的重复项.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+using namespace std;
+//给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+//
+//不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+//
+//元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/remove-element
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+#include <vector>
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int first = 0, second = 0;
+        for (second = 0;second < nums.size();second++) {
+            if (nums[second] != val) {
+                nums[first] = nums[second];
+                first++;
+            }
+        }
+        return first;
+    }
+};
+int main() {
+
+
+	system("pause");
+	return 0;
+}
