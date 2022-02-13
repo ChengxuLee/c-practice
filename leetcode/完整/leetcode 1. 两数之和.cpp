ff88659f379a52c2b4ab06ba5ae4ad05a1428059
ChengxuLee <1695172472@qq.com>
2022-02-13 13:14:43 +0800

    .

--- leetcode/完整/leetcode 1. 两数之和.cpp
+++ leetcode/完整/leetcode 1. 两数之和.cpp
@@ -1,30 +1,43 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
 using namespace std;
-/*给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
-
-不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
-
-示例 1：
-
-输入：nums = [1,1,2]
-输出：2, nums = [1,2]
-解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
-*/
-#include <vector>
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+//
+//你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+//
+//你可以按任意顺序返回答案。
+//
+//
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/two-sum
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+//
+//输入：nums = [2, 7, 11, 15], target = 9
+//输出：[0, 1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回[0, 1] 。
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/two-sum
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-          int first=1;
-          int second=1;
-          if(nums.empty()) return 0;
-            while(second<nums.size()){
-                if(nums[second] != nums[second-1]){
-                    nums[first++]=nums[second]; 
-                }
-                second++;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> p;
+        map<int, int> hash;
+        for (int i = 0;i < nums.size();i++) {
+            hash[nums[i]] = i;
+        }
+        //判断
+        for (int j = 0;j < nums.size();j++) {
+            int targetnum = target - nums[j];
+            if (hash.count(targetnum) != 0 && hash[targetnum] != j) {
+                p.push_back(j);
+                p.push_back(hash[targetnum]);
+                return p;
             }
-          return first;
+        }
+        return p;
     }
 };
 int main() {
